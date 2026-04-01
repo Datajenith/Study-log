ba1edf7a864bba66564570977832ddcaa393c332
Datajenith <coolyoungjae@naver.com>
2026-04-01 12:40:39 +0900

    Update money_log.py

--- money_log.py
+++ money_log.py
@@ -1,1 +1,11 @@
+# 2026-04-01 지출 데이터
+expenses = [
+    {"item": "시내버스", "amount": 1550},
+    {"item": "아메리카노", "amount": 2000}
+]
 
+# 실제로 계산을 수행하는 코드
+total = sum(expense["amount"] for expense in expenses)
+
+# 결과 출력
+print(f"오늘의 총 지출 합계는 {total}원입니다.")
